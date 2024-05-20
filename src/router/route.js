e7c400b784fa7f1510fdd6147902966720ff4dbb
Andhika <78576587+AndhikaGG@users.noreply.github.com>
2024-05-20 11:29:41 +0700

    Update route.js

--- src/router/route.js
+++ src/router/route.js
@@ -5,6 +5,7 @@ const Services = require("../controller/services")
 
 route.get("/", (req, res) => {
     res.send({
+        owner: "Andhika from NDBOTZ",
         endpoint: {
             getOngoingAnime: "/api/v1/ongoing/:page",
             getCompletedAnime: "/api/v1/completed/:page",
